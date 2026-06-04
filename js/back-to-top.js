a76d05a5bcab785f3b63403e9fec2b1599ea950f
xandermeyen <xander.meyen@gmail.com>
2026-06-04 17:11:47 +0200

    terug naar boven knop toegevoegd

---
+++ js/back-to-top.js
@@ -0,0 +1,13 @@
+const backToTopBtn = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopBtn.classList.add('visible');
+    } else {
+        backToTopBtn.classList.remove('visible');
+    }
+});
+
+backToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
